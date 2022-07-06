0d88a79a68f0f5f77f29b8d36f463cbb3a5a991e
David De Carli <josedaviddc@gmail.com>
2022-07-06 02:29:07 -0300

    Corrigiendo el desafío de 'Catálogo con MAPS y Promises'

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Item  from './Item';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function ItemList({items}) {
+function ItemList({items}){
     return (
-        <div>
-            <ul>
-                {items.map((item) =><li><Item key={item.id}>{item.name}</Item></li>)}
-            </ul>
+        <div className='container d-flex justify-content-center bg-info'>
+            <div className='row'>
+                {
+                    items.map((item) => (
+                        <div className='col-md-6' key={item.id}>
+                            <Item title={item.title} imagen={item.pictureUrl} precio={item.price} description={item.description} url={item.url}/>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
-    );
+    )
 }
+
 export default ItemList;
